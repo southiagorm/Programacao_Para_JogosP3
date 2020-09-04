bc82a0497c34d25532ccd09f8a6f22f60b3e12ce
Thiago Rodrigues Medeiros <southiagorm@gmail.com>
2020-09-04 13:46:42 -0300

    Criação do Projeto Nave, GameObjects, Input

---
+++ ProjetoNaveV0.1/Assets/Scripts/NaveController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveController : MonoBehaviour
+{
+    public float speed = 0.01f;
+    private float _h, _v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,speed,0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -speed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed, 0,0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed, 0, 0);
+        }*/
+
+        _h = Input.GetAxis("Horizontal");
+        _v = Input.GetAxis("Vertical");
+
+        transform.Translate(_h*speed,_v*speed,0);
+
+    }
+}
