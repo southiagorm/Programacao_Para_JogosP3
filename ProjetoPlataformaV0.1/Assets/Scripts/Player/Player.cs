15adaccc44caee72f7496698b2ceba490b72fe21
Thiago Rodrigues Medeiros <southiagorm@gmail.com>
2020-11-10 13:37:06 -0300

    Aula 10/11 Sprites, Animação e Controle de Transição de Estados

---
+++ ProjetoPlataformaV0.1/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Animator anim;
+    private float h;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+
+        anim.SetFloat("run", Mathf.Abs(h));
+    }
+}
