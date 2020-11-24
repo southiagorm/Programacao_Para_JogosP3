8160f08a33d75fe1d4de90f382459fe0d910a2c6
Thiago Rodrigues Medeiros <southiagorm@gmail.com>
2020-11-24 08:08:25 -0300

    Aula 23/11 - Animação Pulo, Tilemap e Plataforma flutuante

---
+++ ProjetoPlataformaV0.3/Assets/Scripts/Plataforma.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Plataforma : MonoBehaviour
+{
+    [SerializeField]
+    private Transform pointA, pointB;
+    [SerializeField]
+    private float speed;
+
+    private Transform pointAux;
+    void Start()
+    {
+        transform.position = pointA.position;
+        pointAux = pointB;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,pointAux.position, speed*Time.deltaTime);
+
+        if(transform.position.y <= pointB.position.y)
+        {
+            pointAux = pointA;
+        }else if(transform.position.y >= pointA.position.y)
+        {
+            pointAux = pointB;
+        }
+    }
+}
